17baf2085a4250ad3b09ea7298fe255dc1e6e8da
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2024-02-16 04:13:57 +0000

    Electron spotify login + token
    i think im going to switch this to next and just run it on vercel.

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const { startFastify } = require("./spotifyauth");
+
+if (process.defaultApp) {
+    if (process.argv.length >= 2) {
+        app.setAsDefaultProtocolClient("wiskatron", process.execPath, [
+            path.resolve(process.argv[1]),
+        ]);
+    }
+} else {
+    app.setAsDefaultProtocolClient("wiskatron");
+}
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -7,6 +19,9 @@ const createWindow = () => {
     });
 
     win.loadFile("index.html");
+    // win.webContents.openDevTools();
+
+    startFastify();
 };
 
 app.whenReady().then(() => {
---
+++ spotifyauth.js
@@ -0,0 +1,60 @@
+const fastify = require("fastify")();
+const oauthPlugin = require("@fastify/oauth2");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const config = {
+    access_token: "",
+    refresh_token: "",
+};
+
+const startFastify = () => {
+    fastify.register(oauthPlugin, {
+        name: "Spotify",
+        scope: ["user-read-currently-playing"],
+        credentials: {
+            client: {
+                id: process.env.SPOTIFY_CLIENT_ID,
+                secret: process.env.SPOTIFY_CLIENT_SECRET,
+            },
+            auth: oauthPlugin.SPOTIFY_CONFIGURATION,
+        },
+        startRedirectPath: "/login/spotify",
+        callbackUri: `http://localhost:${process.env.PORT}/login/spotify/callback`,
+    });
+
+    fastify.get("/login/spotify/callback", async (req, reply) => {
+        const result =
+            await fastify.Spotify.getAccessTokenFromAuthorizationCodeFlow(req);
+
+        console.log("The Spotify token is %o", result.token);
+        config.access_token = result.token.access_token;
+        config.refresh_token = result.token.refresh_token;
+        let refreshTimeout = result.token.expires_in;
+        console.log("Refreshing in " + refreshTimeout + " seconds");
+        setInterval(async () => {
+            console.log("GETTING NEW ACCESS TOKEN WITH REFRESH");
+            const result =
+                await fastify.Spotify.getNewAccessTokenUsingRefreshToken(
+                    config
+                );
+            config.access_token = result.token.access_token;
+            console.log("NEW TOKEN:");
+            console.log(config);
+            console.log("Refreshing in " + refreshTimeout + " seconds");
+        }, refreshTimeout * 1000);
+
+        // reply.send({ access_token: config.access_token });
+        reply.redirect(process.env.SPOTIFY_CALLBACK_URI);
+    });
+
+    fastify.listen({ port: 3005 }, () => {
+        console.log("Server listening at http://localhost:3005/login/spotify");
+    });
+};
+
+module.exports = {
+    startFastify,
+    config,
+};
